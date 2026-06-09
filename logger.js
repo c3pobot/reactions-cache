d297318f53e72dc0878c952f0eafb2558001f5e3
scuba75 <scuba75@gmail.com>
2026-06-09 07:28:58 -0500

    update

--- logger.js
+++ logger.js
@@ -1,11 +1,23 @@
 'use strict'
-function getTimeStamp(timestamp){
-  if(!timestamp) timestamp = Date.now()
-  let dateTime = new Date(timestamp)
-  return dateTime.toLocaleString('en-US', { timeZone: 'Etc/GMT+5', hour12: false })
+
+const timestampFormatter = new Intl.DateTimeFormat('en-US', {
+  timeZone: 'Etc/GMT+5',
+  hour12: false,
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+});
+
+function getTimeStamp(timestamp = Date.now()) {
+  return timestampFormatter.format(new Date(timestamp));
 }
+
 module.exports.error = (err)=>{
   console.error(`${getTimeStamp(Date.now())} ERROR [reactions-cache] ${err}`)
+  if(err?.stack) console.error(err)
 }
 module.exports.info = (msg)=>{
   console.log(`${getTimeStamp(Date.now())} INFO [reactions-cache] ${msg}`)
